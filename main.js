840df6eae8198a081505d60cccf7682914ccad0f
Yeet-er <84867114+Yeet-er@users.noreply.github.com>
2021-09-01 15:30:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 menu_list_array = ["Veg Margherita Pizza","BBQ Chicken Pizza","Italian Ultimatum","Make Your Own!"];
 
 function getmenu(){
-var htmldata;
+var htmldata="";
 menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+ menu_list_array[i] + '<br>'
@@ -12,17 +12,19 @@ menu_list_array.sort();
 
 function add_item(){
 var htmldata;
+var imgtags='<img id="im1" src="https://th.bing.com/th/id/OIP.6a_AccS9r1I2fHP71PF3OQHaHQ?pid=ImgDet&rs=1"/>'
+
 var item=document.getElementById("add_item").value;
  menu_list_array.sort();
         htmldata=""
         for(var i=0;i<menu_list_array.length;i++){
             htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
         }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+         document.getElementById("display_addedmenu").innerHTML = htmldata;     
       }
 
 function add_top(){
     var item=document.getElementById("add_item").value;
     menu_list_array.push(item);
-	add_item();
+    add_item();
 }
